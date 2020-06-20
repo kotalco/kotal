24b6ef419a2ff8dd33ed7ff12672790c7c20c1cb
Mostafa Farghaly <keepondev@gmail.com>
2020-06-20 15:57:30 +0400

    refactored network defaulting

---
+++ api/v1alpha1/defaults.go
@@ -0,0 +1,68 @@
+package v1alpha1
+
+var (
+	// DefaultAPIs is the default rpc, ws APIs
+	DefaultAPIs []API = []API{Web3API, ETHAPI, NetworkAPI}
+	// DefaultOrigins is the default origins
+	DefaultOrigins []string = []string{"*"}
+)
+
+// Node defaults
+const (
+	// DefaultHost is the default host
+	DefaultHost = "0.0.0.0"
+	// DefaultP2PPort is the default p2p port
+	DefaultP2PPort uint = 30303
+	// DefaultSyncMode is the default sync mode
+	DefaultSyncMode = FullSynchronization
+	// DefaultRPCPort is the default rpc port
+	DefaultRPCPort uint = 8545
+	// DefaultWSPort is the default ws port
+	DefaultWSPort uint = 8546
+	// DefaultGraphQLPort is the default graphQL port
+	DefaultGraphQLPort uint = 8547
+)
+
+// Genesis block defaults
+const (
+	// DefaultCoinbase is the default coinbase
+	DefaultCoinbase = EthereumAddress("0x0000000000000000000000000000000000000000")
+	// DefaultDifficulty is the default difficulty
+	DefaultDifficulty = HexString("0x1")
+	// DefaultMixHash is the default mix hash
+	DefaultMixHash = Hash("0x0000000000000000000000000000000000000000000000000000000000000000")
+	// DefaultGasLimit is the default gas limit
+	DefaultGasLimit = HexString("0x47b760")
+	// DefaultNonce is the default nonce
+	DefaultNonce = HexString("0x0")
+	// DefaultTimestamp is the default timestamp
+	DefaultTimestamp = HexString("0x0")
+	// DefaultEIP150Hash is the default eip150 hash
+	DefaultEIP150Hash = Hash("0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0")
+)
+
+// Clique engine defaults
+const (
+	// DefaultCliqueBlockPeriod is the default clique block period
+	DefaultCliqueBlockPeriod uint = 15
+	// DefaultCliqueEpochLength is th default clique epoch length
+	DefaultCliqueEpochLength uint = 3000
+)
+
+// IBFT2 engine defaults
+const (
+	// DefaultIBFT2BlockPeriod is the default ibft2 block period
+	DefaultIBFT2BlockPeriod uint = 15
+	// DefaultIBFT2EpochLength is the default ibft2 epoch length
+	DefaultIBFT2EpochLength uint = 3000
+	// DefaultIBFT2RequestTimeout is the default ibft2 request timeout
+	DefaultIBFT2RequestTimeout uint = 10
+	// DefaultIBFT2MessageQueueLimit is the default ibft2 message queue limit
+	DefaultIBFT2MessageQueueLimit uint = 1000
+	// DefaultIBFT2DuplicateMesageLimit is the default ibft2 duplicate message limit
+	DefaultIBFT2DuplicateMesageLimit uint = 100
+	// DefaultIBFT2FutureMessagesLimit is the default ibft2 future message limit
+	DefaultIBFT2FutureMessagesLimit uint = 1000
+	// DefaultIBFT2FutureMessagesMaxDistance is the default ibft2 future message max distance
+	DefaultIBFT2FutureMessagesMaxDistance uint = 10
+)
--- api/v1alpha1/network_defaulting_webhook.go
+++ api/v1alpha1/network_defaulting_webhook.go
@@ -25,11 +25,9 @@ func (r *Network) Default() {
 // DefaultNode defaults a single node
 func (r *Network) DefaultNode(node *Node) {
 	defaultAPIs := []API{Web3API, ETHAPI, NetworkAPI}
-	anyAddress := "0.0.0.0"
-	allOrigins := []string{"*"}
 
 	if node.P2PPort == 0 {
-		node.P2PPort = 30303
+		node.P2PPort = DefaultP2PPort
 	}
 
 	if node.SyncMode == "" {
@@ -38,17 +36,17 @@ func (r *Network) DefaultNode(node *Node) {
 
 	if node.RPC || node.WS || node.GraphQL {
 		if len(node.Hosts) == 0 {
-			node.Hosts = allOrigins
+			node.Hosts = DefaultOrigins
 		}
 
 		if len(node.CORSDomains) == 0 {
-			node.CORSDomains = allOrigins
+			node.CORSDomains = DefaultOrigins
 		}
 	}
 
 	if node.RPC {
 		if node.RPCHost == "" {
-			node.RPCHost = anyAddress
+			node.RPCHost = DefaultHost
 		}
 
 		if node.RPCPort == 0 {
@@ -62,11 +60,11 @@ func (r *Network) DefaultNode(node *Node) {
 
 	if node.WS {
 		if node.WSHost == "" {
-			node.WSHost = anyAddress
+			node.WSHost = DefaultHost
 		}
 
 		if node.WSPort == 0 {
-			node.WSPort = 8546
+			node.WSPort = DefaultWSPort
 		}
 
 		if len(node.WSAPI) == 0 {
@@ -76,11 +74,11 @@ func (r *Network) DefaultNode(node *Node) {
 
 	if node.GraphQL {
 		if node.GraphQLHost == "" {
-			node.GraphQLHost = anyAddress
+			node.GraphQLHost = DefaultHost
 		}
 
 		if node.GraphQLPort == 0 {
-			node.GraphQLPort = 8547
+			node.GraphQLPort = DefaultGraphQLPort
 		}
 	}
 
@@ -89,66 +87,72 @@ func (r *Network) DefaultNode(node *Node) {
 // DefaultGenesis defaults genesis block parameters
 func (r *Network) DefaultGenesis() {
 	if r.Spec.Genesis.Coinbase == "" {
-		r.Spec.Genesis.Coinbase = "0x0000000000000000000000000000000000000000"
+		r.Spec.Genesis.Coinbase = DefaultCoinbase
 	}
 
 	if r.Spec.Genesis.Difficulty == "" {
-		r.Spec.Genesis.Difficulty = "0x1"
+		r.Spec.Genesis.Difficulty = DefaultDifficulty
 	}
 
 	if r.Spec.Genesis.Forks == nil {
 		// all milestones will be activated at block 0
 		r.Spec.Genesis.Forks = &Forks{
-			EIP150Hash: "0x2086799aeebeae135c246c65021c82b4e15a2c451340993aacfd2751886514f0",
+			EIP150Hash: DefaultEIP150Hash,
 		}
 	}
 
 	if r.Spec.Genesis.MixHash == "" {
-		r.Spec.Genesis.MixHash = "0x0000000000000000000000000000000000000000000000000000000000000000"
+		r.Spec.Genesis.MixHash = DefaultMixHash
 	}
 
 	if r.Spec.Genesis.GasLimit == "" {
-		r.Spec.Genesis.GasLimit = "0x47b760"
+		r.Spec.Genesis.GasLimit = DefaultGasLimit
 	}
 
 	if r.Spec.Genesis.Nonce == "" {
-		r.Spec.Genesis.Nonce = "0x0"
+		r.Spec.Genesis.Nonce = DefaultNonce
 	}
 
 	if r.Spec.Genesis.Timestamp == "" {
-		r.Spec.Genesis.Timestamp = "0x0"
+		r.Spec.Genesis.Timestamp = DefaultTimestamp
 	}
 
 	if r.Spec.Consensus == ProofOfAuthority {
+		if r.Spec.Genesis.Clique == nil {
+			r.Spec.Genesis.Clique = &Clique{}
+		}
 		if r.Spec.Genesis.Clique.BlockPeriod == 0 {
-			r.Spec.Genesis.Clique.BlockPeriod = 15
+			r.Spec.Genesis.Clique.BlockPeriod = DefaultCliqueBlockPeriod
 		}
 		if r.Spec.Genesis.Clique.EpochLength == 0 {
-			r.Spec.Genesis.Clique.EpochLength = 3000
+			r.Spec.Genesis.Clique.EpochLength = DefaultCliqueEpochLength
 		}
 	}
 
 	if r.Spec.Consensus == IstanbulBFT {
+		if r.Spec.Genesis.IBFT2 == nil {
+			r.Spec.Genesis.IBFT2 = &IBFT2{}
+		}
 		if r.Spec.Genesis.IBFT2.BlockPeriod == 0 {
-			r.Spec.Genesis.IBFT2.BlockPeriod = 15
+			r.Spec.Genesis.IBFT2.BlockPeriod = DefaultIBFT2BlockPeriod
 		}
 		if r.Spec.Genesis.IBFT2.EpochLength == 0 {
-			r.Spec.Genesis.IBFT2.EpochLength = 3000
+			r.Spec.Genesis.IBFT2.EpochLength = DefaultIBFT2EpochLength
 		}
 		if r.Spec.Genesis.IBFT2.RequestTimeout == 0 {
-			r.Spec.Genesis.IBFT2.RequestTimeout = 10
+			r.Spec.Genesis.IBFT2.RequestTimeout = DefaultIBFT2RequestTimeout
 		}
 		if r.Spec.Genesis.IBFT2.MessageQueueLimit == 0 {
-			r.Spec.Genesis.IBFT2.MessageQueueLimit = 1000
+			r.Spec.Genesis.IBFT2.MessageQueueLimit = DefaultIBFT2MessageQueueLimit
 		}
 		if r.Spec.Genesis.IBFT2.DuplicateMesageLimit == 0 {
-			r.Spec.Genesis.IBFT2.DuplicateMesageLimit = 100
+			r.Spec.Genesis.IBFT2.DuplicateMesageLimit = DefaultIBFT2DuplicateMesageLimit
 		}
 		if r.Spec.Genesis.IBFT2.FutureMessagesLimit == 0 {
-			r.Spec.Genesis.IBFT2.FutureMessagesLimit = 1000
+			r.Spec.Genesis.IBFT2.FutureMessagesLimit = DefaultIBFT2FutureMessagesLimit
 		}
 		if r.Spec.Genesis.IBFT2.FutureMessagesMaxDistance == 0 {
-			r.Spec.Genesis.IBFT2.FutureMessagesMaxDistance = 10
+			r.Spec.Genesis.IBFT2.FutureMessagesMaxDistance = DefaultIBFT2FutureMessagesMaxDistance
 		}
 
 	}
