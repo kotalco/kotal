d07018ad7041ecedbb66c569c7aef63052797bc1
Mostafa Farghaly <keepondev@gmail.com>
2020-11-03 23:46:32 +0200

    added network to filecoin node spec

--- apis/filecoin/v1alpha1/node_types.go
+++ apis/filecoin/v1alpha1/node_types.go
@@ -6,7 +6,24 @@ import (
 
 // NodeSpec defines the desired state of Node
 type NodeSpec struct {
+	// Network is the Filecoin network the node will join and sync
+	Network FilecoinNetwork `json:"network,omitempty"`
 }
+
+// FilecoinNetwork is Filecoin network
+// +kubebuilder:validation:Enum=mainnet;nerpa;butterfly;calibration
+type FilecoinNetwork string
+
+const (
+	// MainNetwork is the Filecoin main network
+	MainNetwork FilecoinNetwork = "mainnet"
+	// NerpaNetwork is the Filecoin main network
+	NerpaNetwork FilecoinNetwork = "nerpa"
+	// ButterflyNetwork is the Filecoin main network
+	ButterflyNetwork FilecoinNetwork = "butterfly"
+	// CalibrationNetwork is the Filecoin main network
+	CalibrationNetwork FilecoinNetwork = "calibration"
+)
 
 // NodeStatus defines the observed state of Node
 type NodeStatus struct {
