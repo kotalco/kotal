b0cbdbeb1291fb87cec819b97a3e99d18cd15e97
Mostafa Farghaly <keepondev@gmail.com>
2020-12-09 17:36:37 +0400

    ethereum2: updated node spec with join and client

--- apis/ethereum2/v1alpha1/node_types.go
+++ apis/ethereum2/v1alpha1/node_types.go
@@ -6,11 +6,30 @@ import (
 
 // NodeSpec defines the desired state of Node
 type NodeSpec struct {
+	// Join is the network to join
+	Join string `json:"join"`
+	// Client is the Ethereum 2.0 client to use
+	Client Ethereum2Client `json:"client,omitempty"`
 }
 
 // NodeStatus defines the observed state of Node
 type NodeStatus struct {
 }
+
+// Ethereum2Client is Ethereum 2.0 client
+// +kubebuilder:validation:Enum=teku;prysm;lighthouse;nimbus
+type Ethereum2Client string
+
+const (
+	// TekuClient is ConsenSys Pegasys Ethereum 2.0 client
+	TekuClient Ethereum2Client = "teku"
+	// PrysmClient is Prysmatic Labs Ethereum 2.0 client
+	PrysmClient Ethereum2Client = "prysm"
+	// LighthouseClient is SigmaPrime Ethereum 2.0 client
+	LighthouseClient Ethereum2Client = "lighthouse"
+	// NimbusClient is Status Ethereum 2.0 client
+	NimbusClient Ethereum2Client = "nimbus"
+)
 
 // +kubebuilder:object:root=true
 
