5070f37b2006d5621e7bec8d94b1fe21669c79d4
Mostafa Farghaly <keepondev@gmail.com>
2021-02-11 19:03:26 +0200

    ethereum2: test validator clients

---
+++ controllers/ethereum2/prysm_validator_client_test.go
@@ -0,0 +1,47 @@
+package controllers
+
+import (
+	"fmt"
+
+	ethereum2v1alpha1 "github.com/kotalco/kotal/apis/ethereum2/v1alpha1"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Prysm Ethereum 2.0 validator client arguments", func() {
+
+	cases := []struct {
+		title     string
+		validator *ethereum2v1alpha1.Validator
+		result    []string
+	}{
+		{
+			title: "mainnet validator client",
+			validator: &ethereum2v1alpha1.Validator{
+				Spec: ethereum2v1alpha1.ValidatorSpec{
+					Client:  ethereum2v1alpha1.PrysmClient,
+					Network: "mainnet",
+				},
+			},
+			result: []string{
+				// update with data dir
+				PrysmAcceptTermsOfUse,
+				"--mainnet",
+			},
+		},
+	}
+
+	for _, c := range cases {
+		func() {
+			cc := c
+			It(fmt.Sprintf("Should create correct client arguments for %s", cc.title), func() {
+				cc.validator.Default()
+				client, err := NewValidatorClient(cc.validator.Spec.Client)
+				Expect(err).To(BeNil())
+				args := client.Args(cc.validator)
+				Expect(args).To(ContainElements(cc.result))
+			})
+		}()
+	}
+
+})
