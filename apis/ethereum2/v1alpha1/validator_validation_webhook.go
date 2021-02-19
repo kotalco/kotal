57deba5093c6009861f6ade468e6ff33ddb15ddd
Mostafa Farghaly <keepondev@gmail.com>
2021-02-19 19:07:02 +0200

    ethereum2: validate walletPassswordSecret is required if validator client is prysm

--- apis/ethereum2/v1alpha1/validator_validation_webhook.go
+++ apis/ethereum2/v1alpha1/validator_validation_webhook.go
@@ -1,7 +1,10 @@
 package v1alpha1
 
 import (
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
@@ -9,18 +12,46 @@ import (
 
 var _ webhook.Validator = &Validator{}
 
+// Validate validates an Ethereum 2.0 validator client
+func (r *Validator) Validate() field.ErrorList {
+	var validatorErrors field.ErrorList
+
+	if r.Spec.Client == PrysmClient && r.Spec.WalletPasswordSecret == "" {
+		err := field.Invalid(field.NewPath("spec").Child("walletPasswordSecret"), r.Spec.WalletPasswordSecret, "must provide walletPasswordSecret if client is prysm")
+		validatorErrors = append(validatorErrors, err)
+	}
+
+	return validatorErrors
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Validator) ValidateCreate() error {
+	var allErrors field.ErrorList
+
 	validatorlog.Info("validate create", "name", r.Name)
 
-	return nil
+	allErrors = append(allErrors, r.Validate()...)
+
+	if len(allErrors) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{}, r.Name, allErrors)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Validator) ValidateUpdate(old runtime.Object) error {
+	var allErrors field.ErrorList
+
 	validatorlog.Info("validate update", "name", r.Name)
 
-	return nil
+	allErrors = append(allErrors, r.Validate()...)
+
+	if len(allErrors) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{}, r.Name, allErrors)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
---
+++ apis/ethereum2/v1alpha1/validator_validation_webhook_test.go
@@ -0,0 +1,118 @@
+package v1alpha1
+
+import (
+	"fmt"
+
+	"github.com/kotalco/kotal/apis/shared"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+var _ = Describe("Ethereum 2.0 validator client validation", func() {
+
+	createCases := []struct {
+		Title     string
+		Validator *Validator
+		Errors    field.ErrorList
+	}{
+		{
+			Title: "Node #1",
+			Validator: &Validator{
+				Spec: ValidatorSpec{
+					Network:  "mainnet",
+					Client:   PrysmClient,
+					Graffiti: "Kotal is amazing",
+					Secrets: []string{
+						"my-validator",
+					},
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.walletPasswordSecret",
+					BadValue: "",
+					Detail:   "must provide walletPasswordSecret if client is prysm",
+				},
+			},
+		},
+	}
+
+	updateCases := []struct {
+		Title        string
+		OldValidator *Validator
+		NewValidator *Validator
+		Errors       field.ErrorList
+	}{
+		{
+			Title: "Node #1",
+			OldValidator: &Validator{
+				Spec: ValidatorSpec{
+					Network:  "mainnet",
+					Client:   TekuClient,
+					Graffiti: "Kotal is amazing",
+					Secrets: []string{
+						"my-validator",
+					},
+				},
+			},
+			NewValidator: &Validator{
+				Spec: ValidatorSpec{
+					Network:  "mainnet",
+					Client:   PrysmClient,
+					Graffiti: "Kotal is amazing",
+					Secrets: []string{
+						"my-validator",
+					},
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.walletPasswordSecret",
+					BadValue: "",
+					Detail:   "must provide walletPasswordSecret if client is prysm",
+				},
+			},
+		},
+	}
+
+	Context("While creating validator client", func() {
+		for _, c := range createCases {
+			func() {
+				cc := c
+				It(fmt.Sprintf("Should validate %s", cc.Title), func() {
+					cc.Validator.Default()
+					err := cc.Validator.ValidateCreate()
+
+					errStatus := err.(*errors.StatusError)
+
+					causes := shared.ErrorsToCauses(cc.Errors)
+
+					Expect(errStatus.ErrStatus.Details.Causes).To(ContainElements(causes))
+				})
+			}()
+		}
+	})
+
+	Context("While updating validator client", func() {
+		for _, c := range updateCases {
+			func() {
+				cc := c
+				It(fmt.Sprintf("Should validate %s", cc.Title), func() {
+					cc.NewValidator.Default()
+					err := cc.NewValidator.ValidateUpdate(cc.OldValidator)
+
+					errStatus := err.(*errors.StatusError)
+
+					causes := shared.ErrorsToCauses(cc.Errors)
+
+					Expect(errStatus.ErrStatus.Details.Causes).To(ContainElements(causes))
+				})
+			}()
+		}
+	})
+
+})
