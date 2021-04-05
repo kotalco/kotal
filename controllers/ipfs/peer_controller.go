98e9c39394242d02fc0ec18d0f10931249f5774c
Mostafa Farghaly <keepondev@gmail.com>
2021-04-06 00:49:26 +0200

    ipfs,peer: reconcile peer persistent volume claim

--- controllers/ipfs/peer_controller.go
+++ controllers/ipfs/peer_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -24,6 +25,7 @@ type PeerReconciler struct {
 // +kubebuilder:rbac:groups=ipfs.kotal.io,resources=peers,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=ipfs.kotal.io,resources=peers/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=watch;get;list;create;update;delete
+// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=watch;get;create;update;list;delete
 
 func (r *PeerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
 	var peer ipfsv1alpha1.Peer
@@ -34,6 +36,10 @@ func (r *PeerReconciler) Reconcile(ctx context.Context, req ctrl.Request) (resul
 	}
 
 	r.updateLabels(&peer)
+
+	if err = r.reconcilePeerPVC(ctx, &peer); err != nil {
+		return
+	}
 
 	if err = r.reconcilePeerStatefulSet(ctx, &peer); err != nil {
 		return
@@ -57,6 +63,43 @@ func (r *PeerReconciler) updateLabels(peer *ipfsv1alpha1.Peer) {
 
 }
 
+func (r *PeerReconciler) reconcilePeerPVC(ctx context.Context, peer *ipfsv1alpha1.Peer) error {
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      peer.Name,
+			Namespace: peer.Namespace,
+		},
+	}
+
+	_, err := ctrl.CreateOrUpdate(ctx, r.Client, pvc, func() error {
+		if err := ctrl.SetControllerReference(peer, pvc, r.Scheme); err != nil {
+			return err
+		}
+		if pvc.CreationTimestamp.IsZero() {
+			r.specPeerPVC(peer, pvc)
+		}
+		return nil
+	})
+
+	return err
+}
+
+// specPeerPVC updates peer persistent volume claim
+func (r *PeerReconciler) specPeerPVC(peer *ipfsv1alpha1.Peer, pvc *corev1.PersistentVolumeClaim) {
+	pvc.ObjectMeta.Labels = peer.Labels
+
+	pvc.Spec = corev1.PersistentVolumeClaimSpec{
+		AccessModes: []corev1.PersistentVolumeAccessMode{
+			corev1.ReadWriteOnce,
+		},
+		Resources: corev1.ResourceRequirements{
+			Requests: corev1.ResourceList{
+				corev1.ResourceStorage: resource.MustParse("100Gi"),
+			},
+		},
+	}
+}
+
 // reconcilePeerStatefulSet reconciles ipfs peer statefulset
 func (r *PeerReconciler) reconcilePeerStatefulSet(ctx context.Context, peer *ipfsv1alpha1.Peer) error {
 
@@ -78,6 +121,7 @@ func (r *PeerReconciler) reconcilePeerStatefulSet(ctx context.Context, peer *ipf
 	return err
 }
 
+// specPeerStatefulSet updates peer statefulset spec
 func (r *PeerReconciler) specPeerStatefulSet(peer *ipfsv1alpha1.Peer, sts *appsv1.StatefulSet) {
 	labels := peer.Labels
 
@@ -129,7 +173,9 @@ func (r *PeerReconciler) specPeerStatefulSet(peer *ipfsv1alpha1.Peer, sts *appsv
 					{
 						Name: "data",
 						VolumeSource: corev1.VolumeSource{
-							EmptyDir: &corev1.EmptyDirVolumeSource{},
+							PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+								ClaimName: peer.Name,
+							},
 						},
 					},
 				},
@@ -143,5 +189,6 @@ func (r *PeerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&ipfsv1alpha1.Peer{}).
 		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.PersistentVolumeClaim{}).
 		Complete(r)
 }
