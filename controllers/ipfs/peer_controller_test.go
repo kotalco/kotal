d272f0dbd411ca5537d7412619c5f3592a93ba47
Mostafa Farghaly <keepondev@gmail.com>
2021-04-18 16:44:22 +0200

    ipfs,peer: test peer controller

---
+++ controllers/ipfs/peer_controller_test.go
@@ -0,0 +1,167 @@
+package controllers
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	ipfsv1alpha1 "github.com/kotalco/kotal/apis/ipfs/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+var _ = Describe("IPFS peer controller", func() {
+	ns := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "ipfs-peer",
+		},
+	}
+
+	key := types.NamespacedName{
+		Name:      "my-peer",
+		Namespace: ns.Name,
+	}
+
+	client := NewIPFSClient()
+
+	spec := ipfsv1alpha1.PeerSpec{
+		APIPort:        3333,
+		GatewayPort:    4444,
+		Routing:        ipfsv1alpha1.DHTClientRouting,
+		SwarmKeySecret: "my-swarm",
+	}
+
+	toCreate := &ipfsv1alpha1.Peer{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      key.Name,
+			Namespace: key.Namespace,
+		},
+		Spec: spec,
+	}
+
+	t := true
+
+	peerOwnerReference := metav1.OwnerReference{
+		APIVersion:         "ipfs.kotal.io/v1alpha1",
+		Kind:               "Peer",
+		Name:               toCreate.Name,
+		Controller:         &t,
+		BlockOwnerDeletion: &t,
+	}
+
+	It(fmt.Sprintf("Should create %s namespace", ns.Name), func() {
+		Expect(k8sClient.Create(context.TODO(), ns)).To(Succeed())
+	})
+
+	It("should create ipfs peer", func() {
+		if os.Getenv("USE_EXISTING_CLUSTER") != "true" {
+			toCreate.Default()
+		}
+		Expect(k8sClient.Create(context.Background(), toCreate)).Should(Succeed())
+	})
+
+	It("Should get ipfs peer", func() {
+		fetched := &ipfsv1alpha1.Peer{}
+		Expect(k8sClient.Get(context.Background(), key, fetched)).To(Succeed())
+		Expect(fetched.Spec).To(Equal(toCreate.Spec))
+		peerOwnerReference.UID = fetched.UID
+		time.Sleep(5 * time.Second)
+	})
+
+	It("Should create peer statefulset with correct arguments", func() {
+		fetched := &appsv1.StatefulSet{}
+		Expect(k8sClient.Get(context.Background(), key, fetched)).To(Succeed())
+		Expect(fetched.OwnerReferences).To(ContainElements(peerOwnerReference))
+		Expect(fetched.Spec.Template.Spec.Containers[0].Image).To(Equal(client.Image()))
+		Expect(fetched.Spec.Template.Spec.Containers[0].Args).To(ContainElements(
+			GoIPFSDaemonArg,
+			GoIPFSRoutingArg,
+			string(ipfsv1alpha1.DHTClientRouting),
+		))
+	})
+
+	It("Should create allocate correct resources to peer statefulset", func() {
+		fetched := &appsv1.StatefulSet{}
+		expectedResources := corev1.ResourceRequirements{
+			Requests: corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse(ipfsv1alpha1.DefaultNodeCPURequest),
+				corev1.ResourceMemory: resource.MustParse(ipfsv1alpha1.DefaultNodeMemoryRequest),
+			},
+			Limits: corev1.ResourceList{
+				corev1.ResourceCPU:    resource.MustParse(ipfsv1alpha1.DefaultNodeCPULimit),
+				corev1.ResourceMemory: resource.MustParse(ipfsv1alpha1.DefaultNodeMemoryLimit),
+			},
+		}
+		Expect(k8sClient.Get(context.Background(), key, fetched)).To(Succeed())
+		Expect(fetched.Spec.Template.Spec.Containers[0].Resources).To(Equal(expectedResources))
+	})
+
+	It("Should create peer configmap", func() {
+		fetched := &corev1.ConfigMap{}
+		Expect(k8sClient.Get(context.Background(), key, fetched)).To(Succeed())
+		Expect(fetched.OwnerReferences).To(ContainElements(peerOwnerReference))
+		Expect(fetched.Data).To(HaveKey("init_ipfs_config.sh"))
+		Expect(fetched.Data).To(HaveKey("config_ipfs.sh"))
+		Expect(fetched.Data).To(HaveKey("copy_swarm_key.sh"))
+	})
+
+	It("Should create peer data persistent volume with correct resources", func() {
+		fetched := &corev1.PersistentVolumeClaim{}
+		Expect(k8sClient.Get(context.Background(), key, fetched)).To(Succeed())
+		Expect(fetched.OwnerReferences).To(ContainElements(peerOwnerReference))
+		expectedResources := corev1.ResourceRequirements{
+			Requests: corev1.ResourceList{
+				corev1.ResourceStorage: resource.MustParse(ipfsv1alpha1.DefaultNodeStorageRequest),
+			},
+		}
+		Expect(fetched.Spec.Resources).To(Equal(expectedResources))
+	})
+
+	It("Should create peer service", func() {
+		fetched := &corev1.Service{}
+		Expect(k8sClient.Get(context.Background(), key, fetched)).To(Succeed())
+		Expect(fetched.OwnerReferences).To(ContainElements(peerOwnerReference))
+		Expect(fetched.Spec.Ports).To(ContainElements(
+			[]corev1.ServicePort{
+				{
+					Name:       "swarm",
+					Port:       4001,
+					TargetPort: intstr.FromInt(4001),
+					Protocol:   corev1.ProtocolTCP,
+				},
+				{
+					Name:       "swarm-udp",
+					Port:       4001,
+					TargetPort: intstr.FromInt(4002),
+					Protocol:   corev1.ProtocolUDP,
+				},
+				{
+					Name:       "api",
+					Port:       int32(3333),
+					TargetPort: intstr.FromInt(int(3333)),
+					Protocol:   corev1.ProtocolTCP,
+				},
+				{
+					Name:       "gateway",
+					Port:       int32(4444),
+					TargetPort: intstr.FromInt(int(4444)),
+					Protocol:   corev1.ProtocolTCP,
+				},
+			},
+		))
+	})
+
+	It(fmt.Sprintf("Should delete %s namespace", ns.Name), func() {
+		Expect(k8sClient.Delete(context.Background(), ns)).To(Succeed())
+	})
+
+})
--- controllers/ipfs/suite_test.go
+++ controllers/ipfs/suite_test.go
@@ -8,6 +8,7 @@ import (
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/rest"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
@@ -37,11 +38,13 @@ var _ = BeforeSuite(func(done Done) {
 	logf.SetLogger(zap.New(zap.WriteTo(GinkgoWriter), zap.UseDevMode(true)))
 
 	By("bootstrapping test environment")
+	// create new test environment
 	testEnv = &envtest.Environment{
-		CRDDirectoryPaths: []string{filepath.Join("..", "config", "crd", "bases")},
+		CRDDirectoryPaths: []string{filepath.Join("..", "..", "config", "crd", "bases")},
 	}
 
 	var err error
+	// start the test environment
 	cfg, err = testEnv.Start()
 	Expect(err).ToNot(HaveOccurred())
 	Expect(cfg).ToNot(BeNil())
@@ -49,14 +52,33 @@ var _ = BeforeSuite(func(done Done) {
 	err = ipfsv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
-	err = ipfsv1alpha1.AddToScheme(scheme.Scheme)
-	Expect(err).NotTo(HaveOccurred())
-
 	// +kubebuilder:scaffold:scheme
 
+	// create new controller manager
+	k8sManager, err := ctrl.NewManager(cfg, ctrl.Options{
+		MetricsBindAddress: "0",
+		Scheme:             scheme.Scheme,
+	})
+	Expect(err).ToNot(HaveOccurred())
+
+	// create new k8s client
 	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
 	Expect(err).ToNot(HaveOccurred())
 	Expect(k8sClient).ToNot(BeNil())
+
+	// start peer reconciler
+	peerReconciler := &PeerReconciler{
+		Client: k8sManager.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("peer"),
+		Scheme: scheme.Scheme,
+	}
+	peerReconciler.SetupWithManager(k8sManager)
+	Expect(err).ToNot(HaveOccurred())
+
+	go func() {
+		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		Expect(err).ToNot(HaveOccurred())
+	}()
 
 	close(done)
 }, 60)
