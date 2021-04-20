4ea85a9f19b7947db3e0b2dc56361145185aa08a
Mostafa Farghaly <keepondev@gmail.com>
2021-04-20 04:08:17 +0200

    ipfs,cluster: created ipfs cluster API resource

---
+++ apis/ipfs/v1alpha1/cluster_peer.go
@@ -0,0 +1,37 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ClusterPeerSpec defines the desired state of ClusterPeer
+type ClusterPeerSpec struct {
+}
+
+// ClusterPeerStatus defines the observed state of ClusterPeer
+type ClusterPeerStatus struct {
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterPeer is the Schema for the clusterpeers API
+type ClusterPeer struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ClusterPeerSpec   `json:"spec,omitempty"`
+	Status ClusterPeerStatus `json:"status,omitempty"`
+}
+
+// +kubebuilder:object:root=true
+
+// ClusterPeerList contains a list of ClusterPeer
+type ClusterPeerList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ClusterPeer `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ClusterPeer{}, &ClusterPeerList{})
+}
