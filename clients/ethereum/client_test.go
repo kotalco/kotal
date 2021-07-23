81f0dace99e17dda5d450ab4d931426eff62e5f2
Mostafa Farghaly <keepondev@gmail.com>
2021-07-23 02:22:59 +0200

    ethereum: test client factory

---
+++ clients/ethereum/client_test.go
@@ -0,0 +1,37 @@
+package ethereum
+
+import (
+	"testing"
+
+	ethereumv1alpha1 "github.com/kotalco/kotal/apis/ethereum/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func TestNewClient(t *testing.T) {
+	node := &ethereumv1alpha1.Node{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node-with-invalid-client",
+		},
+		Spec: ethereumv1alpha1.NodeSpec{
+			NetworkConfig: ethereumv1alpha1.NetworkConfig{
+				Network: ethereumv1alpha1.MainNetwork,
+			},
+			Client: ethereumv1alpha1.EthereumClient("nokia"),
+		},
+	}
+
+	client, err := NewClient(node)
+	if err == nil {
+		t.Error("expecting an error")
+	}
+
+	if client != nil {
+		t.Error("expecting client to be nil")
+	}
+
+	expected := "client nokia is not supported"
+	got := err.Error()
+	if expected != got {
+		t.Errorf("expected error message to be: %s", expected)
+	}
+}
