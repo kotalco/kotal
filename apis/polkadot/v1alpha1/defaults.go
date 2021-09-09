79bd23644e57f3cafc65bf22acc098be06acaebb
Mostafa Farghaly <keepondev@gmail.com>
2021-09-09 23:27:31 +0200

    polkadot: default node resources

---
+++ apis/polkadot/v1alpha1/defaults.go
@@ -0,0 +1,17 @@
+package v1alpha1
+
+// Resources
+const (
+	// DefaultNodeCPURequest is the cpu requested by polkadot node
+	DefaultNodeCPURequest = "4"
+	// DefaultNodeCPULimit is the cpu limit for polkadot node
+	DefaultNodeCPULimit = "8"
+
+	// DefaultNodeMemoryRequest is the memory requested by polkadot node
+	DefaultNodeMemoryRequest = "4Gi"
+	// DefaultNodeMemoryLimit is the memory limit for polkadot node
+	DefaultNodeMemoryLimit = "8Gi"
+
+	// DefaultNodeStorageRequest is the Storage requested by polkadot node
+	DefaultNodeStorageRequest = "80Gi"
+)
