779b5760b94236a9d947a91a6e61a091821c79b7
Mostafa Farghaly <keepondev@gmail.com>
2021-10-22 03:04:28 +0200

    ethereum: update hyperledger besu client to v21.7.4

--- clients/ethereum/besu_client.go
+++ clients/ethereum/besu_client.go
@@ -19,7 +19,7 @@ const (
 	// EnvBesuImage is the environment variable used for hyperledger besu image
 	EnvBesuImage = "BESU_IMAGE"
 	// DefaultBesuImage is hyperledger besu image
-	DefaultBesuImage = "hyperledger/besu:21.7.1"
+	DefaultBesuImage = "hyperledger/besu:21.7.4"
 	// BesuHomeDir is besu docker image home directory
 	BesuHomeDir = "/opt/besu"
 )
