15af4ca461038509ea7697adecd615251e684cbc
Mostafa Farghaly <keepondev@gmail.com>
2021-11-03 18:57:31 +0200

    chainlink: reconcile node statefulset using client

--- controllers/chainlink/node_controller.go
+++ controllers/chainlink/node_controller.go
@@ -3,11 +3,14 @@ package controllers
 import (
 	"context"
 
+	chainlinkv1alpha1 "github.com/kotalco/kotal/apis/chainlink/v1alpha1"
+	chainlinkClients "github.com/kotalco/kotal/clients/chainlink"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	chainlinkv1alpha1 "github.com/kotalco/kotal/apis/chainlink/v1alpha1"
 )
 
 // NodeReconciler reconciles a Node object
@@ -20,7 +23,86 @@ type NodeReconciler struct {
 // +kubebuilder:rbac:groups=chainlink.kotal.io,resources=nodes/status,verbs=get;update;patch
 
 func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+
+	var node chainlinkv1alpha1.Node
+
+	if err = r.Client.Get(ctx, req.NamespacedName, &node); err != nil {
+		err = client.IgnoreNotFound(err)
+		return
+	}
+
+	if err = r.reconcileStatefulset(ctx, &node); err != nil {
+		return
+	}
+
 	return
+}
+
+// reconcileStatefulset reconciles node statefulset
+func (r *NodeReconciler) reconcileStatefulset(ctx context.Context, node *chainlinkv1alpha1.Node) error {
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      node.Name,
+			Namespace: node.Namespace,
+		},
+	}
+
+	client := chainlinkClients.NewClient(node)
+
+	img := client.Image()
+	command := client.Command()
+	args := client.Args()
+	env := client.Env()
+	homeDir := client.HomeDir()
+
+	_, err := ctrl.CreateOrUpdate(ctx, r.Client, sts, func() error {
+		if err := ctrl.SetControllerReference(node, sts, r.Scheme); err != nil {
+			return err
+		}
+		if err := r.specStatefulSet(node, sts, img, homeDir, command, args, env); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
+// specStatefulSet updates node statefulset spec
+func (r *NodeReconciler) specStatefulSet(node *chainlinkv1alpha1.Node, sts *appsv1.StatefulSet, image, homeDir string, command, args []string, env []corev1.EnvVar) error {
+
+	// TODO: use shared node labels
+	labels := map[string]string{
+		"name": node.Name,
+	}
+
+	sts.ObjectMeta.Labels = labels
+
+	sts.Spec = appsv1.StatefulSetSpec{
+		Selector: &metav1.LabelSelector{
+			MatchLabels: labels,
+		},
+		ServiceName: node.Name,
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: labels,
+			},
+			Spec: corev1.PodSpec{
+				// TODO: use shared security context
+				Containers: []corev1.Container{
+					{
+						Name:    "node",
+						Image:   image,
+						Command: command,
+						Args:    args,
+						Env:     env,
+					},
+				},
+			},
+		},
+	}
+
+	return nil
 }
 
 func (r *NodeReconciler) SetupWithManager(mgr ctrl.Manager) error {
