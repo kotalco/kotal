2309187a83cd374359f248e5a79631253d91e210
Mostafa Farghaly <keepondev@gmail.com>
2021-11-06 03:27:22 +0200

    chainlink: update spec with resources

--- apis/chainlink/v1alpha1/node.go
+++ apis/chainlink/v1alpha1/node.go
@@ -1,6 +1,7 @@
 package v1alpha1
 
 import (
+	"github.com/kotalco/kotal/apis/shared"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -26,6 +27,8 @@ type NodeSpec struct {
 	KeystorePasswordSecretName string `json:"keystorePasswordSecretName"`
 	// APICredentials is api credentials
 	APICredentials APICredentials `json:"apiCredentials"`
+	// Resources is node compute and storage resources
+	shared.Resources `json:"resources,omitempty"`
 }
 
 // NodeStatus defines the observed state of Node
