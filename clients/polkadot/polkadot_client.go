04cb741c3708c9b4754abd464298ebdab00ea24b
Mostafa Farghaly <keepondev@gmail.com>
2021-12-05 03:12:51 +0400

    polkadot: upgrade client

--- clients/polkadot/polkadot_client.go
+++ clients/polkadot/polkadot_client.go
@@ -21,7 +21,7 @@ const (
 	// EnvPolkadotImage is the environment variable used for polkadot client image
 	EnvPolkadotImage = "POLKADOT_IMAGE"
 	// DefaultPolkadotImage is the default polkadot client image
-	DefaultPolkadotImage = "parity/polkadot:v0.9.12"
+	DefaultPolkadotImage = "parity/polkadot:v0.9.13"
 	//  PolkadotHomeDir is go ipfs image home dir
 	PolkadotHomeDir = "/polkadot"
 )
