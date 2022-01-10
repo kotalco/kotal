e2e9373e19f2ef41efa367eacfaebd411a35cc13
Mostafa Farghaly <keepondev@gmail.com>
2022-01-10 15:39:01 +0200

    near: create node defaulting and validation webhooks

--- main.go
+++ main.go
@@ -180,12 +180,19 @@ func main() {
 			os.Exit(1)
 		}
 	}
+
 	if err = (&nearcontroller.NodeReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "Node")
 		os.Exit(1)
+	}
+	if enableWebhooks {
+		if err = (&nearv1alpha1.Node{}).SetupWebhookWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create webhook", "webhook", "Node")
+			os.Exit(1)
+		}
 	}
 	// +kubebuilder:scaffold:builder
 
