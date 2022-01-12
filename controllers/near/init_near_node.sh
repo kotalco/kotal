1e9e59a75acbbb867483dc8d3310069d2ece91cc
Mostafa Farghaly <keepondev@gmail.com>
2022-01-13 01:19:15 +0200

    near: update node init script with home dir

--- controllers/near/init_near_node.sh
+++ controllers/near/init_near_node.sh
@@ -6,7 +6,7 @@ set -e
 if [ -z "$(ls -A $KOTAL_DATA_PATH/genesis.json)" ]
 then
     echo "Initializing NEAR node"
-	neard init --chain-id $KOTAL_NEAR_NETWORK --download-genesis --download-config
+	neard --home $KOTAL_DATA_PATH init --chain-id $KOTAL_NEAR_NETWORK --download-genesis --download-config
 else
 	echo "NEAR node has already been initialized before!"
 fi
