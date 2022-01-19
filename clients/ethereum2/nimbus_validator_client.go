80bff26cf3d0d4ba3d0c1580a29ee4af607f0782
Mostafa Farghaly <keepondev@gmail.com>
2022-01-19 15:37:44 +0200

    ethereum2: update nimbus client to v1.6.0

--- clients/ethereum2/nimbus_validator_client.go
+++ clients/ethereum2/nimbus_validator_client.go
@@ -21,7 +21,7 @@ const (
 	// EnvNimbusValidatorImage is the environment variable used for Status Ethereum 2.0 validator client image
 	EnvNimbusValidatorImage = "NIMBUS_VALIDATOR_CLIENT_IMAGE"
 	// DefaultNimbusValidatorImage is the default Status Ethereum 2.0 validator client image
-	DefaultNimbusValidatorImage = "kotalco/nimbus:v1.5.5"
+	DefaultNimbusValidatorImage = "kotalco/nimbus:v1.6.0"
 )
 
 // HomeDir returns container home directory
