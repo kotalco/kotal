5c08422a7e3f33103c01211152e8a4238e247311
Mostafa Farghaly <keepondev@gmail.com>
2022-03-19 14:49:32 +0200

    bitcoin: reconcile node service

--- controllers/bitcoin/node_controller.go
+++ controllers/bitcoin/node_controller.go
@@ -9,6 +9,7 @@ import (
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
@@ -25,7 +26,7 @@ type NodeReconciler struct {
 // +kubebuilder:rbac:groups=bitcoin.kotal.io,resources=nodes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=bitcoin.kotal.io,resources=nodes/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=watch;get;list;create;update;delete
-// +kubebuilder:rbac:groups=core,resources=persistentvolumeclaims,verbs=watch;get;create;update;list;delete
+// +kubebuilder:rbac:groups=core,resources=services;persistentvolumeclaims,verbs=watch;get;create;update;list;delete
 
 // Reconcile Bitcoin node
 func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
@@ -44,6 +45,10 @@ func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (resul
 	shared.UpdateLabels(&node, "bitcoind")
 
 	if err = r.reconcilePVC(ctx, &node); err != nil {
+		return
+	}
+
+	if err = r.reconcileService(ctx, &node); err != nil {
 		return
 	}
 
@@ -97,6 +102,46 @@ func (r *NodeReconciler) specPVC(node *bitcoinv1alpha1.Node, pvc *corev1.Persist
 			Requests: request,
 		},
 	}
+}
+
+// reconcileService reconciles Bitcoin node service
+func (r *NodeReconciler) reconcileService(ctx context.Context, node *bitcoinv1alpha1.Node) error {
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      node.Name,
+			Namespace: node.Namespace,
+		},
+	}
+
+	_, err := ctrl.CreateOrUpdate(ctx, r.Client, svc, func() error {
+		if err := ctrl.SetControllerReference(node, svc, r.Scheme); err != nil {
+			return err
+		}
+		r.specService(node, svc)
+		return nil
+	})
+
+	return err
+}
+
+// specService updates Bitcoin node service spec
+func (r *NodeReconciler) specService(node *bitcoinv1alpha1.Node, svc *corev1.Service) {
+	labels := node.Labels
+
+	svc.ObjectMeta.Labels = labels
+
+	svc.Spec.Ports = []corev1.ServicePort{}
+
+	if node.Spec.RPC {
+		svc.Spec.Ports = append(svc.Spec.Ports, corev1.ServicePort{
+			Name:       "rpc",
+			Port:       int32(node.Spec.RPCPort),
+			TargetPort: intstr.FromInt(int(node.Spec.RPCPort)),
+			Protocol:   corev1.ProtocolTCP,
+		})
+	}
+
+	svc.Spec.Selector = labels
 }
 
 // reconcileStatefulset reconciles node statefulset
@@ -192,5 +237,6 @@ func (r *NodeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&bitcoinv1alpha1.Node{}).
 		Owns(&corev1.PersistentVolumeClaim{}).
 		Owns(&appsv1.StatefulSet{}).
+		Owns(&corev1.Service{}).
 		Complete(r)
 }
