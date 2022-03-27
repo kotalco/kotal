cd00c32a1566760f81fb01be873dc9844ad91671
Mostafa Farghaly <keepondev@gmail.com>
2022-03-27 15:52:28 +0200

    stacks: default node resources

---
+++ apis/stacks/v1alpha1/defaults.go
@@ -0,0 +1,17 @@
+package v1alpha1
+
+// Resources
+const (
+	// DefaultNodeCPURequest is the cpu requested by Stacks node
+	DefaultNodeCPURequest = "2"
+	// DefaultNodeCPULimit is the cpu limit for Stacks node
+	DefaultNodeCPULimit = "4"
+
+	// DefaultNodeMemoryRequest is the memory requested by Stacks node
+	DefaultNodeMemoryRequest = "4Gi"
+	// DefaultNodeMemoryLimit is the memory limit for Stacks node
+	DefaultNodeMemoryLimit = "8Gi"
+
+	// DefaultNodeStorageRequest is the Storage requested by Stacks node
+	DefaultNodeStorageRequest = "100Gi"
+)
