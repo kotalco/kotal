a85086213dcf4cc8a21f96ae554f6b491740bbf2
Mostafa Farghaly <keepondev@gmail.com>
2022-03-28 15:09:27 +0200

    bitcoin: validate nodes resources

--- apis/stacks/v1alpha1/node_validation_webhook.go
+++ apis/stacks/v1alpha1/node_validation_webhook.go
@@ -1,7 +1,10 @@
 package v1alpha1
 
 import (
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
@@ -11,16 +14,33 @@ var _ webhook.Validator = &Node{}
 
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Node) ValidateCreate() error {
+	var allErrors field.ErrorList
+
 	nodelog.Info("validate create", "name", r.Name)
 
-	return nil
+	allErrors = append(allErrors, r.Spec.Resources.ValidateCreate()...)
+
+	if len(allErrors) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{}, r.Name, allErrors)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Node) ValidateUpdate(old runtime.Object) error {
+	var allErrors field.ErrorList
+	oldNode := old.(*Node)
+
 	nodelog.Info("validate update", "name", r.Name)
 
-	return nil
+	allErrors = append(allErrors, r.Spec.Resources.ValidateUpdate(&oldNode.Spec.Resources)...)
+
+	if len(allErrors) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{}, r.Name, allErrors)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
