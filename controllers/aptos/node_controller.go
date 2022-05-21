21eef88d164a86cf4be8e94956141491a0a96d2d
Mostafa Farghaly <keepondev@gmail.com>
2022-05-21 02:57:25 +0200

    aptos: reconcile node statefulset

--- controllers/aptos/node_controller.go
+++ controllers/aptos/node_controller.go
@@ -3,11 +3,15 @@ package controllers
 import (
 	"context"
 
+	aptosv1alpha1 "github.com/kotalco/kotal/apis/aptos/v1alpha1"
+	aptosClients "github.com/kotalco/kotal/clients/aptos"
+	"github.com/kotalco/kotal/controllers/shared"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-
-	aptosv1alpha1 "github.com/kotalco/kotal/apis/aptos/v1alpha1"
 )
 
 // NodeReconciler reconciles a Node object
@@ -18,13 +22,92 @@ type NodeReconciler struct {
 
 // +kubebuilder:rbac:groups=aptos.kotal.io,resources=nodes,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=aptos.kotal.io,resources=nodes/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=watch;get;list;create;update;delete
 
 func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+	var node aptosv1alpha1.Node
+
+	if err = r.Client.Get(ctx, req.NamespacedName, &node); err != nil {
+		err = client.IgnoreNotFound(err)
+		return
+	}
+
+	//TODO: default node if hooks are disabled
+
+	shared.UpdateLabels(&node, "aptos-core")
+
+	if err = r.reconcileStatefulset(ctx, &node); err != nil {
+		return
+	}
+
 	return
+}
+
+// reconcileStatefulset reconciles node statefulset
+func (r *NodeReconciler) reconcileStatefulset(ctx context.Context, node *aptosv1alpha1.Node) error {
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      node.Name,
+			Namespace: node.Namespace,
+		},
+	}
+
+	client := aptosClients.NewClient(node)
+
+	img := client.Image()
+	homeDir := client.HomeDir()
+	cmd := client.Command()
+	args := client.Args()
+	env := client.Env()
+
+	_, err := ctrl.CreateOrUpdate(ctx, r.Client, sts, func() error {
+		if err := ctrl.SetControllerReference(node, sts, r.Scheme); err != nil {
+			return err
+		}
+		if err := r.specStatefulSet(node, sts, img, homeDir, env, cmd, args); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
+// specStatefulSet updates node statefulset spec
+func (r *NodeReconciler) specStatefulSet(node *aptosv1alpha1.Node, sts *appsv1.StatefulSet, img, homeDir string, env []corev1.EnvVar, cmd, args []string) error {
+
+	sts.ObjectMeta.Labels = node.Labels
+
+	sts.Spec = appsv1.StatefulSetSpec{
+		Selector: &metav1.LabelSelector{
+			MatchLabels: node.Labels,
+		},
+		ServiceName: node.Name,
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: node.Labels,
+			},
+			Spec: corev1.PodSpec{
+				SecurityContext: shared.SecurityContext(),
+				Containers: []corev1.Container{
+					{
+						Name:    "node",
+						Image:   img,
+						Command: cmd,
+						Args:    args,
+						Env:     env,
+					},
+				},
+			},
+		},
+	}
+
+	return nil
 }
 
 func (r *NodeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&aptosv1alpha1.Node{}).
+		Owns(&appsv1.StatefulSet{}).
 		Complete(r)
 }
