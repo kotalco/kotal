759b607c51d6c22e76b172e6930913967556f51f
Mostafa Farghaly <keepondev@gmail.com>
2022-05-23 15:00:33 +0200

    aptos: validate peerId is required if nodePrivateKeySecretName is provided and vice versa

--- apis/aptos/v1alpha1/node_validation_webhook.go
+++ apis/aptos/v1alpha1/node_validation_webhook.go
@@ -1,7 +1,10 @@
 package v1alpha1
 
 import (
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
 
@@ -9,18 +12,57 @@ import (
 
 var _ webhook.Validator = &Node{}
 
+// validate shared validation logic for create and update resources
+func (r *Node) validate() field.ErrorList {
+	var nodeErrors field.ErrorList
+
+	if r.Spec.NodePrivateKeySecretName != "" && r.Spec.PeerId == "" {
+		err := field.Invalid(field.NewPath("spec").Child("peerId"), r.Spec.PeerId, "must provide peerId if nodePrivateKeySecretName is provided")
+		nodeErrors = append(nodeErrors, err)
+	}
+
+	if r.Spec.PeerId != "" && r.Spec.NodePrivateKeySecretName == "" {
+		err := field.Invalid(field.NewPath("spec").Child("nodePrivateKeySecretName"), r.Spec.NodePrivateKeySecretName, "must provide nodePrivateKeySecretName if peerId is provided")
+		nodeErrors = append(nodeErrors, err)
+	}
+
+	return nodeErrors
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Node) ValidateCreate() error {
+	var allErrors field.ErrorList
+
 	nodelog.Info("validate create", "name", r.Name)
 
-	return nil
+	allErrors = append(allErrors, r.validate()...)
+
+	if len(allErrors) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{}, r.Name, allErrors)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Node) ValidateUpdate(old runtime.Object) error {
+	var allErrors field.ErrorList
+	oldNode := old.(*Node)
+
 	nodelog.Info("validate update", "name", r.Name)
 
-	return nil
+	allErrors = append(allErrors, r.validate()...)
+
+	if r.Spec.Network != oldNode.Spec.Network {
+		err := field.Invalid(field.NewPath("spec").Child("network"), r.Spec.Network, "field is immutable")
+		allErrors = append(allErrors, err)
+	}
+
+	if len(allErrors) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{}, r.Name, allErrors)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
---
+++ apis/aptos/v1alpha1/node_validation_webhook_test.go
@@ -0,0 +1,194 @@
+package v1alpha1
+
+import (
+	"fmt"
+
+	"github.com/kotalco/kotal/apis/shared"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+)
+
+var _ = Describe("Aptos node validation", func() {
+	createCases := []struct {
+		Title  string
+		Node   *Node
+		Errors field.ErrorList
+	}{
+		{
+			Title: "missing peerId",
+			Node: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network:                  Devnet,
+					NodePrivateKeySecretName: "my-private-key",
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.peerId",
+					BadValue: "",
+					Detail:   "must provide peerId if nodePrivateKeySecretName is provided",
+				},
+			},
+		},
+		{
+			Title: "missing nodePrivateKeySecretName",
+			Node: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network: Devnet,
+					PeerId:  "76c8ca8bb75d1abd853fc17b70cc72cb78a63425fa85be96743825d93cc57d6f",
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.nodePrivateKeySecretName",
+					BadValue: "",
+					Detail:   "must provide nodePrivateKeySecretName if peerId is provided",
+				},
+			},
+		},
+	}
+
+	updateCases := []struct {
+		Title   string
+		OldNode *Node
+		NewNode *Node
+		Errors  field.ErrorList
+	}{
+		{
+			Title: "updated network",
+			OldNode: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network: Devnet,
+				},
+			},
+			NewNode: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network: Testnet,
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.network",
+					BadValue: Testnet,
+					Detail:   "field is immutable",
+				},
+			},
+		},
+		{
+			Title: "missing peerId",
+			OldNode: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network:                  Devnet,
+					NodePrivateKeySecretName: "my-private-key",
+					PeerId:                   "76c8ca8bb75d1abd853fc17b70cc72cb78a63425fa85be96743825d93cc57d6f",
+				},
+			},
+			NewNode: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network:                  Devnet,
+					NodePrivateKeySecretName: "my-private-key",
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.peerId",
+					BadValue: "",
+					Detail:   "must provide peerId if nodePrivateKeySecretName is provided",
+				},
+			},
+		},
+		{
+			Title: "missing nodePrivateKeySecretName",
+			OldNode: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network:                  Devnet,
+					NodePrivateKeySecretName: "my-private-key",
+					PeerId:                   "76c8ca8bb75d1abd853fc17b70cc72cb78a63425fa85be96743825d93cc57d6f",
+				},
+			},
+			NewNode: &Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "my-node",
+				},
+				Spec: NodeSpec{
+					Network: Devnet,
+					PeerId:  "76c8ca8bb75d1abd853fc17b70cc72cb78a63425fa85be96743825d93cc57d6f",
+				},
+			},
+			Errors: field.ErrorList{
+				{
+					Type:     field.ErrorTypeInvalid,
+					Field:    "spec.nodePrivateKeySecretName",
+					BadValue: "",
+					Detail:   "must provide nodePrivateKeySecretName if peerId is provided",
+				},
+			},
+		},
+	}
+
+	Context("While creating node", func() {
+		for _, c := range createCases {
+			func() {
+				cc := c
+				It(fmt.Sprintf("Should validate %s", cc.Title), func() {
+					cc.Node.Default()
+					err := cc.Node.ValidateCreate()
+
+					errStatus := err.(*errors.StatusError)
+
+					causes := shared.ErrorsToCauses(cc.Errors)
+
+					Expect(errStatus.ErrStatus.Details.Causes).To(ContainElements(causes))
+				})
+			}()
+		}
+	})
+
+	Context("While updating node", func() {
+		for _, c := range updateCases {
+			func() {
+				cc := c
+				It(fmt.Sprintf("Should validate %s", cc.Title), func() {
+					cc.OldNode.Default()
+					cc.NewNode.Default()
+					err := cc.NewNode.ValidateUpdate(cc.OldNode)
+
+					errStatus := err.(*errors.StatusError)
+
+					causes := shared.ErrorsToCauses(cc.Errors)
+
+					Expect(errStatus.ErrStatus.Details.Causes).To(ContainElements(causes))
+				})
+			}()
+		}
+	})
+
+})
