4fe9c43d1445bdb7389292bbb94aa551601f753a
Mostafa Farghaly <keepondev@gmail.com>
2022-06-03 01:28:35 +0400

    k8s: update manager arguments

--- main.go
+++ main.go
@@ -57,8 +57,8 @@ func init() {
 func main() {
 	var metricsAddr string
 	var enableLeaderElection bool
-	flag.StringVar(&metricsAddr, "metrics-addr", ":8080", "The address the metric endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "enable-leader-election", false,
+	flag.StringVar(&metricsAddr, "metrics-bind-addres", ":8080", "The address the metric endpoint binds to.")
+	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
 
