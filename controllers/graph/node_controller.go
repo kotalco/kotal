0d988a16a73c0fcb71e814113cfc78696e47cebd
Mostafa Farghaly <keepondev@gmail.com>
2022-08-28 15:24:10 +0200

    graph: reconcile node statefulset

--- controllers/graph/node_controller.go
+++ controllers/graph/node_controller.go
@@ -4,6 +4,12 @@ import (
 	"context"
 
 	graphv1alpha1 "github.com/kotalco/kotal/apis/graph/v1alpha1"
+	graphClients "github.com/kotalco/kotal/clients/graph"
+	"github.com/kotalco/kotal/controllers/shared"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -18,14 +24,117 @@ type NodeReconciler struct {
 //+kubebuilder:rbac:groups=graph.kotal.io,resources=nodes,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=graph.kotal.io,resources=nodes/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=graph.kotal.io,resources=nodes/finalizers,verbs=update
+//+kubebuilder:rbac:groups=apps,resources=statefulsets,verbs=watch;get;list;create;update;delete
 
 func (r *NodeReconciler) Reconcile(ctx context.Context, req ctrl.Request) (result ctrl.Result, err error) {
+	var node graphv1alpha1.Node
+
+	if err = r.Client.Get(ctx, req.NamespacedName, &node); err != nil {
+		err = client.IgnoreNotFound(err)
+		return
+	}
+
+	// TODO: default the node if webhooks are disabled
+
+	shared.UpdateLabels(&node, "graph-node")
+
+	if err = r.reconcileStatefulset(ctx, &node); err != nil {
+		return
+	}
+
 	return
+}
+
+// reconcileStatefulset reconciles node statefulset
+func (r *NodeReconciler) reconcileStatefulset(ctx context.Context, node *graphv1alpha1.Node) error {
+	sts := &appsv1.StatefulSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      node.Name,
+			Namespace: node.Namespace,
+		},
+	}
+
+	client := graphClients.NewClient(node)
+
+	img := client.Image()
+	homeDir := client.HomeDir()
+	cmd := client.Command()
+	args := client.Args()
+	env := client.Env()
+
+	_, err := ctrl.CreateOrUpdate(ctx, r.Client, sts, func() error {
+		if err := ctrl.SetControllerReference(node, sts, r.Scheme); err != nil {
+			return err
+		}
+		if err := r.specStatefulSet(node, sts, img, homeDir, env, cmd, args); err != nil {
+			return err
+		}
+		return nil
+	})
+
+	return err
+}
+
+// specStatefulSet updates node statefulset spec
+func (r *NodeReconciler) specStatefulSet(node *graphv1alpha1.Node, sts *appsv1.StatefulSet, img, homeDir string, env []corev1.EnvVar, cmd, args []string) error {
+
+	sts.ObjectMeta.Labels = node.Labels
+
+	sts.Spec = appsv1.StatefulSetSpec{
+		Selector: &metav1.LabelSelector{
+			MatchLabels: node.Labels,
+		},
+		ServiceName: node.Name,
+		Template: corev1.PodTemplateSpec{
+			ObjectMeta: metav1.ObjectMeta{
+				Labels: node.Labels,
+			},
+			Spec: corev1.PodSpec{
+				SecurityContext: shared.SecurityContext(),
+				Containers: []corev1.Container{
+					{
+						Name:    "node",
+						Image:   img,
+						Command: cmd,
+						Args:    args,
+						Env:     env,
+						Resources: corev1.ResourceRequirements{
+							Requests: corev1.ResourceList{
+								corev1.ResourceCPU:    resource.MustParse("1"),
+								corev1.ResourceMemory: resource.MustParse("1Gi"),
+							},
+							Limits: corev1.ResourceList{
+								corev1.ResourceCPU:    resource.MustParse("2"),
+								corev1.ResourceMemory: resource.MustParse("2Gi"),
+							},
+						},
+						VolumeMounts: []corev1.VolumeMount{
+							{
+								Name:      "data",
+								MountPath: shared.PathData(homeDir),
+							},
+						},
+					},
+				},
+				Volumes: []corev1.Volume{
+					{
+						Name: "data",
+						VolumeSource: corev1.VolumeSource{
+							EmptyDir: &corev1.EmptyDirVolumeSource{},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
 func (r *NodeReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&graphv1alpha1.Node{}).
+		Owns(&appsv1.StatefulSet{}).
 		Complete(r)
 }
