e61cb25378950a55c712b3e730f016eb722a986b
Mostafa Farghaly <keepondev@gmail.com>
2022-11-27 01:15:35 +0200

    ethereum2: update default lighthouse client to v3.3.0

--- clients/ethereum2/lighthouse_beacon_node.go
+++ clients/ethereum2/lighthouse_beacon_node.go
@@ -21,7 +21,7 @@ const (
 	// EnvLighthouseBeaconNodeImage is the environment variable used for SigmaPrime Ethereum 2.0 beacon node image
 	EnvLighthouseBeaconNodeImage = "LIGHTHOUSE_BEACON_NODE_IMAGE"
 	// DefaultLighthouseBeaconNodeImage is the default SigmaPrime Ethereum 2.0 beacon node image
-	DefaultLighthouseBeaconNodeImage = "kotalco/lighthouse:v3.2.1"
+	DefaultLighthouseBeaconNodeImage = "kotalco/lighthouse:v3.3.0"
 )
 
 // HomeDir returns container home directory
