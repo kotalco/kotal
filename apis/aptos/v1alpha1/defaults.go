0cdf2b9223686980be03b25217e44205eed5e10a
Mostafa Farghaly <keepondev@gmail.com>
2023-05-14 00:20:39 +0300

    aptos: update default images sha256 tags

--- apis/aptos/v1alpha1/defaults.go
+++ apis/aptos/v1alpha1/defaults.go
@@ -18,11 +18,11 @@ const (
 
 const (
 	// DefaultAptosCoreDevnetImage is the default Aptos core Devnet client image
-	DefaultAptosCoreDevnetImage = "aptoslabs/validator:devnet"
+	DefaultAptosCoreDevnetImage = "aptoslabs/validator@sha256:604c279f52c6c99dbe238229507c074758c2178b455c18890ef58a2fc38ccc80"
 	// DefaultAptosCoreMainnetImage is the default Aptos core Mainnet client image
-	DefaultAptosCoreMainnetImage = "aptoslabs/validator:mainnet"
+	DefaultAptosCoreMainnetImage = "aptoslabs/validator@sha256:21ae8e3bb7eba6f9e42e0f464640b19425a73d24836f6f2eb5260fdcba132475"
 	// DefaultAptosCoreTestnetImage is the default Aptos core Testnet client image
-	DefaultAptosCoreTestnetImage = "aptoslabs/validator:testnet"
+	DefaultAptosCoreTestnetImage = "aptoslabs/validator@sha256:c109ab86066fc35cbff5d7f57340ea6da9ed480896d08cd1bbd30c3dec683033"
 )
 
 const (
